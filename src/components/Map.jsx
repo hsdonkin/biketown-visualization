79e3caa372859b5237e9041d8b913f8311cc9d32
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-12-02 13:41:08 -0800

    track most ridden bike on map with polylines

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import L from 'leaflet';
 import provider from 'leaflet-providers';
 
+import log from './../logs/0995BETRUE2018TRAINER.json';
+
 class Map extends React.Component {
   constructor(props) {
     super(props);
@@ -10,13 +12,19 @@ class Map extends React.Component {
   componentDidMount = () => {
     const mymap = L.map('mapid').setView([45.537, -122.687], 12);
     L.tileLayer.provider('Stamen.Toner').addTo(mymap);
+    log.items.forEach(item => {
+      const coordinates = item.route.path.coordinates;
+      const polyline = L.polyline(coordinates, { color: '#FC4C02' }).addTo(
+        mymap
+      );
+    });
   };
 
   render() {
     return (
       <div>
         <h2>Map</h2>
-        <div id="mapid" style={{ height: 600, width: 800 }}></div>
+        <div id="mapid" style={{ height: 700, width: 1200 }}></div>
       </div>
     );
   }
