6bb263869b51348be47a3556a3a636a17d175462
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-12-03 15:26:04 -0800

    fetch station status

--- src/components/MapContainer.jsx
+++ src/components/MapContainer.jsx
@@ -19,23 +19,61 @@ const axios = require('axios');
 class MapContainer extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { loading: true, error: false, freeBikeStatus: [] };
+    this.state = {
+      loading: true,
+      error: false,
+      freeBikeStatus: [],
+      stationStatus: [],
+      windowSize: { height: 600, width: 1200 }
+    };
   }
 
   componentDidMount = () => {
-    axios({
+    // find out what height / width to set the map to
+    if (
+      this.state.windowSize.height != window.innerHeight ||
+      this.state.windowSize.width != window.innerWidth
+    ) {
+      this.setState({
+        windowSize: { height: window.innerHeight, width: window.innerWidth }
+      });
+    }
+
+    const fetchBikeStatus = axios({
       url: `http://biketownpdx.socialbicycles.com/opendata/free_bike_status.json`
-    })
-      .then(response => {
-        const freeBikeStatus = response.data;
-        this.setState({ loading: false, freeBikeStatus: freeBikeStatus });
+    });
+
+    const fetchStationStatus = axios({
+      url: 'http://biketownpdx.socialbicycles.com/opendata/station_status.json'
+    });
+
+    Promise.all([fetchBikeStatus, fetchStationStatus])
+      .then(responses => {
+        const freeBikeStatus = responses[0].data;
+        const stationStatus = responses[1].data;
+        this.setState({
+          loading: false,
+          freeBikeStatus: freeBikeStatus,
+          stationStatus: stationStatus
+        });
         console.log(this.state);
       })
       .catch(err => this.setState({ loading: false, error: true }));
   };
 
   render() {
+    // default map position
     const position = [45.51, -122.66];
+
+    // handle window resize
+    window.onresize = e => {
+      this.setState({
+        windowSize: {
+          height: e.currentTarget.innerHeight,
+          width: e.currentTarget.innerWidth
+        }
+      });
+    };
 
     let freeBikeMarkers;
     if (this.state.loading === false && this.state.error === false) {
@@ -45,14 +83,13 @@ class MapContainer extends React.Component {
         return (
           <Marker
             position={[bike.lat, bike.lon]}
-            title={bike.name}
             icon={L.icon({
               iconUrl: bikeMarker,
               iconSize: [30, 30]
             })}
           >
-            <Tooltip>
-              <span className={'bike-marker-tooltip'}>{bike.name}</span>
+            <Tooltip className={'bike-marker-tooltip'}>
+              <span>{bike.name}</span>
             </Tooltip>
           </Marker>
         );
@@ -66,7 +103,10 @@ class MapContainer extends React.Component {
         zoom={13}
         minZoom={12}
         maxZoom={17}
-        style={{ height: 700, width: 1200 }}
+        style={{
+          height: this.state.windowSize.height - 75,
+          width: this.state.windowSize.width
+        }}
       >
         <TileLayer
           url="http://tile.stamen.com/toner/{z}/{x}/{y}.png"
