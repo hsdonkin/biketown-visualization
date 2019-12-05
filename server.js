071e3946bfe0393f6d05181abd5d43e1f50d5718
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-12-05 13:23:45 -0800

    fix error require newrelic

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const bodyParser = require('body-parser');
 const path = require('path');
 
 // new relic webpack solution
+const newrelic = require('newrelic');
 newrelic.instrumentLoadedModule(
   'express', // the module's name, as a string
   express // the module instance
