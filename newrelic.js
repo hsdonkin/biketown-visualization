d02ad81ecbe0febb5b652ed5dbd9453d071bcf94
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-12-05 11:11:42 -0800

    change app name in newrelic

--- newrelic.js
+++ newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['Biketown Map'],
+  app_name: ['Biketown Map Heroku'],
   /**
    * Your New Relic license key.
    */
