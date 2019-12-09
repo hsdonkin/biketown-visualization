6cdd599eb4b6737ddaef3fa90b7352da70f6bd1d
Hugh Sherman  Donkin <hsdonkin@gmail.com>
2019-12-09 15:07:56 -0800

    add drop shadow and collapsing icon

--- src/components/Legend.jsx
+++ src/components/Legend.jsx
@@ -4,69 +4,92 @@ import stationIcon from './../assets/station-icon.svg';
 import serviceAreaIcon from './../assets/service-area-path.svg';
 import specialAreaIcon from './../assets/special-area-icon.svg';
 
-const Legend = props => {
-  return (
-    <div className={'legend'}>
-      <h3>Legend</h3>
-      <span className="legend-item">
-        <span className="img-holder">
-          <img src={bicycleIcon} alt="Bikes" />
-        </span>
+class Legend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      showLegend: false
+    };
+  }
+  render() {
+    if (this.state.showLegend === true) {
+      return (
+        <div className={'legend'}>
+          <h3>Legend</h3>
+          <span
+            className="legend-collapsed-icon"
+            onClick={() => this.setState({ showLegend: false })}
+          ></span>
+          <span className="legend-item">
+            <span className="img-holder">
+              <img src={bicycleIcon} alt="Bikes" />
+            </span>
 
-        <input
-          onClick={() => {
-            props.handleToggle('showBikes');
-          }}
-          type="checkbox"
-          defaultChecked
-        />
+            <input
+              onClick={() => {
+                this.props.handleToggle('showBikes');
+              }}
+              type="checkbox"
+              defaultChecked
+            />
 
-        <p>Bikes</p>
-      </span>
-      <span className="legend-item">
-        <span className="img-holder">
-          <img src={stationIcon} alt="Stations" />
-        </span>
+            <p>Bikes</p>
+          </span>
+          <span className="legend-item">
+            <span className="img-holder">
+              <img src={stationIcon} alt="Stations" />
+            </span>
 
-        <input
-          onClick={() => {
-            props.handleToggle('showStations');
-          }}
-          type="checkbox"
-          defaultChecked
-        />
+            <input
+              onClick={() => {
+                this.props.handleToggle('showStations');
+              }}
+              type="checkbox"
+              defaultChecked
+            />
 
-        <p>Stations</p>
-      </span>
-      <span className="legend-item">
-        <span className="img-holder">
-          <img src={serviceAreaIcon} alt="Service Areas" />
-        </span>
+            <p>Stations</p>
+          </span>
+          <span className="legend-item">
+            <span className="img-holder">
+              <img src={serviceAreaIcon} alt="Service Areas" />
+            </span>
 
-        <input
-          onClick={() => {
-            props.handleToggle('showServiceArea');
-          }}
-          type="checkbox"
-          defaultChecked
-        />
+            <input
+              onClick={() => {
+                this.props.handleToggle('showServiceArea');
+              }}
+              type="checkbox"
+              defaultChecked
+            />
 
-        <p>Service Area</p>
-      </span>
-      <span className="legend-item">
-        <span className="img-holder">
-          <img src={specialAreaIcon} alt="Special Areas" />
-        </span>
-        <input
-          onClick={() => {
-            props.handleToggle('showSpecialAreas');
-          }}
-          type="checkbox"
-        />
-        <p>Special Areas</p>
-      </span>
-    </div>
-  );
-};
+            <p>Service Area</p>
+          </span>
+          <span className="legend-item">
+            <span className="img-holder">
+              <img src={specialAreaIcon} alt="Special Areas" />
+            </span>
+            <input
+              onClick={() => {
+                this.props.handleToggle('showSpecialAreas');
+              }}
+              type="checkbox"
+            />
+            <p>Special Areas</p>
+          </span>
+        </div>
+      );
+    } else {
+      return (
+        <div className="legend-collapsed">
+          <span
+            className="legend-collapsed-icon"
+            onClick={() => this.setState({ showLegend: true })}
+          ></span>
+        </div>
+      );
+    }
+  }
+}
 
 export default Legend;
